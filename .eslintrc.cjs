8eac66eb4e9d4f578289e6a5176189045e6d78a8
Felipe Emos <felipe.emos.computacao@gmail.com>
2024-01-27 02:24:56 -0300

    fix(eslint): unused vars enums false positives

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,8 @@ const config = {
     "solid/no-destructure": "warn",
     "solid/jsx-no-undef": "error",
     "prettier/prettier": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
   },
 };
 
